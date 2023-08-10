bced1978f76e7e3c9596069420150ea4ca83e0fd
youngeunkim <pkhj286@gmail.com>
2023-08-10 20:46:12 +0900

    Proptypes

--- practice/src/MyComponent.js
+++ practice/src/MyComponent.js
@@ -1,3 +1,5 @@
+import PropTypes from "prop-types";
+
 const MyComponent = ({ name, children }) => {
   // props -> properties를 줄인 표현으로 컴포넌트 속성을 설정할 때 사용하는 요소
   return (
@@ -11,6 +13,10 @@ const MyComponent = ({ name, children }) => {
 
 MyComponent.defaultProps = {
   name: "이름을 정해주세용",
+};
+
+MyComponent.propTypes = {
+  name: PropTypes.string,
 };
 
 export default MyComponent;
