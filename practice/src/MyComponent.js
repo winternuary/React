c36e18a5e75a69c5a9c3fadbbe1013cbdb0348a1
youngeunkim <pkhj286@gmail.com>
2023-08-10 20:39:26 +0900

    이게 젤 쉽다

--- practice/src/MyComponent.js
+++ practice/src/MyComponent.js
@@ -1,6 +1,5 @@
-const MyComponent = (props) => {
+const MyComponent = ({ name, children }) => {
   // props -> properties를 줄인 표현으로 컴포넌트 속성을 설정할 때 사용하는 요소
-  const { name, children } = props;
   return (
     <div>
       안녕하세요, 제 이름은 {name} 입니다.
