1b4d96561c9cf9d318631f4d3673bef2a4e7b8d7
youngeunkim <pkhj286@gmail.com>
2023-08-10 21:00:57 +0900

    state에 여러 값이 있을 때

--- practice/src/practice/state.js
+++ practice/src/practice/state.js
@@ -7,14 +7,16 @@ class Counter extends Component {
     // state의 초기값 설정
     this.state = {
       number: 0,
+      fixedNumber: 0,
     };
   }
 
   render() {
-    const { number } = this.state;
+    const { number, fixedNumber } = this.state;
     return (
       <div>
         <h1>{number}</h1>
+        <h2>바뀌지 않는 값: {fixedNumber}</h2>
         <button
           onClick={() => {
             this.setState({ number: number + 1 });
