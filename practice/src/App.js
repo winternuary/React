a477763ec3445e145d4e1b477569d2ef13be84e0
youngeunkim <pkhj286@gmail.com>
2023-08-10 20:58:16 +0900

    클래스형 컴포넌트 state

--- practice/src/App.js
+++ practice/src/App.js
@@ -1,7 +1,13 @@
 import MyComponent from "./practice/MyComponent";
+import Counter from "./practice/state";
 
 function App() {
-  return <MyComponent>나는야 애깅</MyComponent>;
+  return (
+    <div>
+      <MyComponent>나는야 애깅</MyComponent>
+      <Counter />
+    </div>
+  );
 }
 
 export default App;
---
+++ practice/src/practice/state.js
@@ -0,0 +1,31 @@
+//클래스형 컴포넌트의 state
+import { Component } from "react";
+
+class Counter extends Component {
+  constructor(props) {
+    super(props);
+    // state의 초기값 설정
+    this.state = {
+      number: 0,
+    };
+  }
+
+  render() {
+    const { number } = this.state;
+    return (
+      <div>
+        <h1>{number}</h1>
+        <button
+          onClick={() => {
+            this.setState({ number: number + 1 });
+          }}
+        >
+          {" "}
+          +1{" "}
+        </button>
+      </div>
+    );
+  }
+}
+
+export default Counter;
