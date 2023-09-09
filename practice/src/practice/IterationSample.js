11760f77f0121697875e04fdf851b72862c242f0
youngeunkim <pkhj286@gmail.com>
2023-09-09 20:50:31 +0900

    key값 부여

--- practice/src/practice/IterationSample.js
+++ practice/src/practice/IterationSample.js
@@ -1,6 +1,6 @@
 const IterationSample = () => {
     const names = ['눈사람', '얼음', '눈', '바람'];
-    const nameList = names.map(name => <li>{name}</li>);
+    const nameList = names.map((name, index) => <li key={index}>{name}</li>);
     return <ul>{nameList}</ul>
 }
 
