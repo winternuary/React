4e7500cd8c1b9281ddc79bc6d2fd44210a342c55
youngeunkim <pkhj286@gmail.com>
2023-09-09 21:27:52 +0900

    데이터 추가

--- practice/src/practice/IterationSample.js
+++ practice/src/practice/IterationSample.js
@@ -1,17 +1,34 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 const IterationSample = () => {
-    const [names, setNames] = useState([
-        { id: 1, text: '눈사람'},
-        { id: 2, text: '얼음'},
-        { id: 3, text: '눈'},
-        { id: 4, text: '바람'},
-    ]);
-    const [inputText, setinputText] = useState('');
-    const [nextId, setNextId] = useState(5);
+  const [names, setNames] = useState([
+    { id: 1, text: "눈사람" },
+    { id: 2, text: "얼음" },
+    { id: 3, text: "눈" },
+    { id: 4, text: "바람" },
+  ]);
+  const [inputText, setinputText] = useState("");
+  const [nextId, setNextId] = useState(5);
 
-    const namesList = names.map(name => <li key={name.id}>{name.text}</li>);
-    return <ul>{namesList}</ul>;
-}
+  const onChange = (e) => setinputText(e.target.value);
+  const onClick = () => {
+    const nextNames = names.concat({
+      id: nextId,
+      text: inputText,
+    });
+    setNextId(nextId + 1);
+    setNames(nextNames);
+    setinputText("");
+  };
+
+  const namesList = names.map((name) => <li key={name.id}>{name.text}</li>);
+  return (
+    <>
+      <input value={inputText} onChange={onChange} />
+      <button onClick={onClick}>추가</button>
+      <ul>{namesList}</ul>;
+    </>
+  );
+};
 
 export default IterationSample;
