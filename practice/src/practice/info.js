3be7e86912273390d30a1accc7e07bd107c125ca
youngeunkim <pkhj286@gmail.com>
2023-11-15 22:25:57 +0900

    useEffect 적용하기

--- practice/src/practice/info.js
+++ practice/src/practice/info.js
@@ -1,9 +1,16 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const Info = () => {
     const [name, setName] = useState("");
     const [nickname, setNickname] = useState("");
-
+    useEffect(() =>{
+        console.log("렌더링이 완료되었습니다.");
+        console.log({
+            name,
+            nickname
+        });
+    });
+    
     const onCangeName = e => {
         setName(e.target.value);
     };
