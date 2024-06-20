950fb1a1b5a8fe151d37dd2e800d6905081aff02
zeroeuni <pkhj286@gmail.com>
2024-06-20 10:07:17 +0900

    chore : 폴더 삭제

--- Toolkit/src/app/store.js
+++ Toolkit/src/app/store.js
@@ -10,17 +10,14 @@ export const counterSlice = createSlice({
       state.value += 1;
     },
     decrement: (state) => {
-      //sate에서 1 감소
       state.value -= 1;
     },
     incrementByAmount: (state, action) => {
-      // sate를 값만큼 증가시키기
       state.value += action.payload;
     },
   },
 });
 
-// Export actions
 export const { increment, decrement, incrementByAmount } = counterSlice.actions;
 
 export const store = configureStore({
